4cb441c502a0017b32c5c11f6e261670b3e465cf
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2021-11-19 19:06:12 +1300

    create home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import gift from "./assets/gift.gif";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wishlist</h1>
+      <p>What gifts do you want?</p>
+      <img src={gift} alt="gift" />
+      <br />
+      <a target="_blank" href="https://icons8.com/icon/g7FGqx8h2AxR/gift">
+        Gift
+      </a>{" "}
+      icon by{" "}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
     </div>
   );
 }
