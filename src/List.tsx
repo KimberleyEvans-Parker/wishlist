64eab45b33f152e93daa8127af47466fcb1896ca
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2022-02-28 23:12:18 +1300

    Add content and buyer vs owner view

--- src/List.tsx
+++ src/List.tsx
@@ -4,15 +4,50 @@ import { useParams } from "react-router";
 import ListItem from "./components/ListItem";
 
 const List = () => {
-    const params = useParams();
-    const id = params.id;
+  const params = useParams();
+  const id = params.id;
+  const buyerView = false;
 
-    return (
-        <div>{id}
-            <ListItem buyerView={true} title="Pony" description="I want a pony" quantity={5} bought={2} />
-            <ListItem buyerView={true} title="Pony" description="I want a pony" quantity={5} bought={2} />
-        </div>
-      );
-}
+  return (
+    <div>
+      {id}
+      <ListItem
+        buyerView={buyerView}
+        title="Pony"
+        description="I want a pony"
+        quantity={1}
+        bought={0}
+      />
+      <ListItem
+        buyerView={buyerView}
+        title="Potato"
+        description="I have a grand plan to make a potato server and all that I'm missing is a potato.  I know it's possible because my tutor had them in a doc he shared with me."
+        quantity={1}
+        bought={1}
+      />
+      <ListItem
+        buyerView={buyerView}
+        title="Things"
+        description="I reaaaallllllyyy reaaaallllyyy reaaallllyyy want a bunch of things. Like a lot of things.  "
+        quantity={100}
+        bought={2}
+      />
+      <ListItem
+        buyerView={buyerView}
+        title="Lorem"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        quantity={3}
+        bought={0}
+      />
+      <ListItem
+        buyerView={buyerView}
+        title="Pancakes"
+        description="The good stuff only"
+        quantity={50}
+        bought={25}
+      />
+    </div>
+  );
+};
 
 export default List;
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,28 +1,40 @@
 import React, { useState } from "react";
+import Cart from "../assets/cart.png";
+import Bin from "../assets/bin.png";
 
 // buyerView: boolean, title: string, description: string, quantity: number, bought: number
 const ListItem = (props: any) => {
-    const [expanded, setExpanded] = useState(false);
-  
-    return (
-        <div className={`list-item-container ${expanded && "height-auto"}`}>
-            <div className="list-item-bar">
-                <div className="circle">
-                    {props?.quantity}
-                </div>
-                <h1 className="list-item-title">{props?.title}</h1>
-                <div className={`circle float-right ${expanded || "rotate"}`} onClick={() => setExpanded(!expanded)}>
-                    {"🞃"}
-                </div>
-            </div>
-            {true &&
-                <div className={`content ${expanded && "grow"}`}>
-                    {props?.description}
-                </div>
-            }
+  const [expanded, setExpanded] = useState(false);
+
+  if (props?.buyerView && props?.quantity - props?.bought == 0) return <></>;
+
+  return (
+    <div className={`list-item-container ${expanded && "height-auto"}`}>
+      <div className="list-item-bar">
+        <div className="circle">
+          {props?.buyerView ? props?.quantity - props?.bought : props?.quantity}
         </div>
-    );
-  };
-  
-  export default ListItem;
-  
+        <h1 className="list-item-title">{props?.title}</h1>
+        <div
+          className={`circle float-right ${expanded || "rotate"}`}
+          onClick={() => setExpanded(!expanded)}
+        >
+          {"🞃"}
+        </div>
+      </div>
+      {true && (
+        <div className={`content ${expanded && "grow"}`}>
+          {/* <div className="circle">{props?.quantity}</div> */}
+          <div className="description-container">
+            <p className="description">{props?.description}</p>
+          </div>
+          <div className="circle">
+            <img src={props?.buyerView ? Cart : Bin} className="icon" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ListItem;
